79ae06a2e14ca437d681891de63e11e50fcf2981
P. Envall <petter.envall@gmail.com>
2023-04-13 12:09:13 +0200

    debug

--- scripts/updateBasePath.js
+++ scripts/updateBasePath.js
@@ -4,27 +4,31 @@ import fs from "fs";
 const basePath = process.argv[2] || null;
 
 if (!basePath) {
-  console.error(`[${process.argv[1]}] No base path provided - exiting!`);
-  process.exit(1);
+    console.error(`[${process.argv[1]}] No base path provided - exiting!`);
+    process.exit(1);
 }
 
 processFiles("html");
 
 async function findFiles(extension) {
-  const files = FileHound.create().paths("./docs").ext(extension).find();
-  return files;
+    const files = FileHound.create().paths("./docs").ext(extension).find();
+    return files;
 }
 
 async function processFiles(extension = "html") {
-  const files = await findFiles(extension);
-  files.forEach((fileName) => {
-    updateHref(fileName);
-  });
+    const files = await findFiles(extension);
+    files.forEach((fileName) => {
+        updateHref(fileName);
+    });
 }
 
 function updateHref(fileName) {
-  const html = fs.readFileSync(fileName, "utf8");
-  const html2 = html.replace(/href="\//g, `href=\"${basePath}\/`);
-  fs.writeFileSync(fileName, html2);
-  console.log("Wrote path updates to", fileName);
+    const html = fs.readFileSync(fileName, "utf8");
+    const html2 = html.replace(/href="\//g, `href=\"${basePath}\/`);
+    fs.writeFileSync(fileName, html2);
+    console.log(
+        "Wrote path updates to",
+        fileName,
+        `using basePath [${basePath}]`
+    );
 }
